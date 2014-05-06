49825bd1536b870057a5c58d213e65ad6fccc725
Hai Nguyen <hhainguyen@gmail.com>
2014-05-06 18:02:33 +0100

    modified:   js/curios-map-view-handler.js 	Add support for map search/all markers viewing

--- js/curios-map-view-handler.js
+++ js/curios-map-view-handler.js
@@ -1,6 +1,6 @@
 /*
  * curios-map-view-handler.js
- * 
+ *
  * Copyright (c) 2011-2014, University of Aberdeen, Hai Nguyen (hai.nguyen@abdn.ac.uk). All rights reserved.
  *
  * CURIOS: Linked Data CMS is free software: you can redistribute it and/or
@@ -16,35 +16,72 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software. If not, see <http://www.gnu.org/licenses/>.
  */
-var osMap, dragControl, marker, mapMarker;
-(function($) {
+(function ($) {
     Drupal.behaviors.mapview_single = {
-        attach: function(context, settings) {
-	// set name for coordinate fields (for example geo:lat, hc:northing...). Check the configuration file under options/map_settings
-	  var 	x = settings.curiosMap.mapXName, 
-    	      	y = settings.curiosMap.mapYName,
-	      	defaultZoom = settings.curiosMap.defaultZoom;	
-		x=x.replace('_','-');
-		
-		var X = $("div[class$="+x+"] span:last-child").html();
-		if(!!X)
-		{	y=y.replace('_','-');			
-			var Y = $("div[class$="+y+"] span:last-child").html();
-			$("div[class$=curios-summary-row]").after("<div id=osmap></div>")
-			$('#osmap').css("width", "100%");
-			$('#osmap').css("height", "400px"); 
-			var marker = new OpenSpace.MapPoint(X,Y);
-			osMap = new OpenSpace.Map("osmap");
-			osMap.setCenter(marker, defaultZoom);
-			osMap.createMarker(marker);
-		}
-           
+        attach: function (context, settings) {
+            // set name for coordinate fields (for example geo:lat, hc:northing...). Check the configuration file under options/map_settings
+            var x = settings.curiosMap.mapXName,
+                y = settings.curiosMap.mapYName,
+                defaultZoom = settings.curiosMap.defaultZoom;
+            x = x.replace('_', '-');
+
+            var X = $("div[class$=" + x + "] span:last-child").html();
+            if (!!X) {
+                y = y.replace('_', '-');
+                var Y = $("div[class$=" + y + "] span:last-child").html();
+                $("div[class$=curios-summary-row]").after("<div id=osmap></div>");
+                $('#osmap').css("width", "100%");
+                $('#osmap').css("height", "400px");
+                var marker = new OpenSpace.MapPoint(X, Y);
+                var osMap = new OpenSpace.Map("osmap");
+                osMap.setCenter(marker, defaultZoom);
+                osMap.createMarker(marker);
+            }
+
+
+
+
+            // This function creates the map 
+
+            $(document).ready(function () {
+
+                // Create new map
+                $(".view-map-view").after("<div id=multimap></div>");
+                $('#multimap').css("width", "100%");
+                $('#multimap').css("height", "600px");
+                var multimap = new OpenSpace.Map('multimap');
+
+                var pos;
+                var places = $(".curios-place").map(function () {
+                    return this.innerHTML;
+                }).get();
+                if (places.length > 0) {
+                    for (var i = 0; i < places.length; i++)
+
+                    {
+                        var columns = places[i].split(',');
+                        if (columns.length == 4) {
+                            var x = parseFloat(columns[1]);
+                            var y = parseFloat(columns[2]);
+                            pos = new OpenSpace.MapPoint(x, y);
+                            var popupText = '<a href="subjects/' + columns[0] + '">' + columns[3] + '</a>';
+                            multimap.createMarker(pos, null, popupText);
+
+                        }
+                    }
+                }
+                multimap.setCenter(pos, defaultZoom);
+                // marker clustering
+                var clusterControl = new OpenSpace.Control.ClusterManager();
+                multimap.addControl(clusterControl);
+                clusterControl.activate();
+
+            });
+
+
+
         }
     }
 
 
 })(jQuery);
-
-
-
-
